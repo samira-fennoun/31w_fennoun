cb0ddf0b0322cdd4180a56e2cbc7a8dcba778186
Fennoun <e2295531@cmaisonneuve.qc.ca>
2022-10-19 14:29:22 -0400

    ajout de dossier composants et creer dedans le fichier nav_primaire

--- header.php
+++ header.php
@@ -26,8 +26,12 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'igc31w' ); ?></a>
 
 	<header id="masthead" class="site__header">
-		<?php  wp_nav_menu(array(
-			"menu"=>"primaire"
+		<?php  
+		wp_nav_menu(array(
+
+			"menu"=>"primaire",
+			"container"=>"nav",
+			"container_class"=>"menu__primaire"
 		)) 
 		?>
 		<div class="site__branding">
