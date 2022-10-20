829e315acdfcfcf3acdc4f6daea397d491ae740f
Fennoun <e2295531@cmaisonneuve.qc.ca>
2022-10-20 16:13:47 -0400

    ajout de fonction pour filter les choix du menu sidebar

--- functions.php
+++ functions.php
@@ -183,3 +183,23 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 */
+
+function igc31w_filtre_choix_menu($obj_menu, $arg){
+    //echo "/////////////////  obj_menu";
+    // var_dump($obj_menu);
+    //  echo "/////////////////  arg";
+    //  var_dump($arg);
+ 
+    if ($arg->menu == "aside"){
+    foreach($obj_menu as $cle => $value)
+    {
+      //  print_r($value);
+       $value->title = substr($value->title,7);
+       $value->title = wp_trim_words($value->title,3,"...");
+        //echo $value->title . '<br>';
+     } 
+    }
+    //die();
+    return $obj_menu;
+}
+add_filter("wp_nav_menu_objects","igc31w_filtre_choix_menu", 10,2);
