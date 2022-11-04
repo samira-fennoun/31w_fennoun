c435025d04a9f7c621feb62921b8f54f11a48c8c
e2295531 <e2295531@cmaisonneuve.qc.ca>
2022-11-03 22:30:54 -0300

    c11 ajout  nouveau aside

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -12,60 +13,73 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+
+    <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'igc31w' ); ?></a>
+    <?php wp_body_open(); ?>
+    <div id="page" class="site">
+        <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'igc31w'); ?></a>
 
-	<header id="masthead" class="site__header">
-		<?php  
-		wp_nav_menu(array(
+        <header id="masthead" class="site__header">
+            <?php
+			wp_nav_menu(array(
 
-			"menu"=>"primaire",
-			"container"=>"nav",
-			"container_class"=>"menu__primaire"
-		)) 
-		?>
-		<div class="site__branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
+				"menu" => "primaire",
+				"container" => "nav",
+				"container_class" => "menu__primaire"
+			))
+			?>
+            <div class="site__branding">
+                <?php
+				the_custom_logo();
+				if (is_front_page() && is_home()) :
 				?>
-				<h1 class="site__title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
+                <h1 class="site__title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                        rel="home"><?php bloginfo('name'); ?></a></h1>
+                <?php
+				else :
 				?>
-				<p class="site__title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$igc31w_description = get_bloginfo( 'description', 'display' );
-			if ( $igc31w_description || is_customize_preview() ) :
+                <p class="site__title"><a href="<?php echo esc_url(home_url('/')); ?>"
+                        rel="home"><?php bloginfo('name'); ?></a></p>
+                <?php
+				endif;
+				$igc31w_description = get_bloginfo('description', 'display');
+				if ($igc31w_description || is_customize_preview()) :
 				?>
-				<p class="site__description"><?php echo $igc31w_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site__branding -->
+                <p class="site__description"><?php echo $igc31w_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
+													?></p>
+                <?php endif; ?>
+            </div><!-- .site__branding -->
 
-		
-	</header><!-- #masthead -->
 
-	<aside class="site__sidebar">
-		<h2>Menu side bar</h2>
+        </header><!-- #masthead -->
 
-		<?php  
-		wp_nav_menu(array(
+        <aside class="site__menu">
+            <h2>Menu aside</h2>
 
-			"menu"=>"aside",
-			"container"=>"nav",
-			"container_class"=>"menu__aside"
-		)) 
-		?>
+            <?php
+			wp_nav_menu(array(
 
-	</aside>
+				"menu" => "aside",
+				"container" => "nav",
+				"container_class" => "menu__aside"
+			))
+			?>
+
+        </aside>
+
+        <aside class="site__sidebar">
+            <h6>Calendrier</h6>
+            <?php get_sidebar('aside-1'); ?>
+            <?php get_sidebar('aside-2'); ?>
+
+        </aside>
+
+        </aside>
