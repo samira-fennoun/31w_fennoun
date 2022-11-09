1a152cab44902ffc61e3b2a6fbf59be0e4c32b3b
e2295531 <e2295531@cmaisonneuve.qc.ca>
2022-11-09 19:54:41 -0400

    c12 ajout le fichier front-page.php

---
+++ front-page.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package igc31w
+ */
+
+get_header();
+
+?>
+<main class="site__main">
+    <code>front-page.php</code>
+    <?php
+    if (have_posts()) :
+        /* Start the Loop */
+        while (have_posts()) :
+            the_post(); ?>
+    <h1><?= get_the_title(); ?></h1>
+
+    <?php the_content();
+            $le_permalien = "<a href='" . get_the_permalink() . "'>Suite</a>";
+            ?>
+
+    <blockquote><?php the_excerpt(); ?></blockquote>
+    <blockquote><?= wp_trim_words(get_the_excerpt(), 5, $le_permalien); ?></blockquote>
+
+
+
+    <?php
+        endwhile;
+    endif;
+    ?>
+</main><!-- #main -->
+<?php
+get_footer();
