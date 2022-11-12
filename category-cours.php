b857070f2566fd8909aae2d570833e9c53a4f466
samira fennoun <101664372+e2295531@users.noreply.github.com>
2022-11-12 13:12:11 -0500

    debut de tp2

--- category-cours.php
+++ category-cours.php
@@ -19,46 +19,51 @@ get_header();
 <h1>category-cour.php</h1>
 <main class="site__main">
 
-	<?php
+    <?php
 	if (have_posts()) :
 
 		/* Start the Loop */
 		while (have_posts()) :
 			the_post();
-			
-			$titre=get_the_title();
-			$code_cours=substr($titre,0,7);
-			$heure_cours=strpos($titre,"(");
-			$heure_cours=substr($titre,$heure_cours);
-			$titre=substr($titre,8);
-			$titre=substr($titre,0,strpos($titre,"(")-strlen($titre));
+
+			$titre = get_the_title();
+			$code_cours = substr($titre, 0, 7);
+			$heure_cours = strpos($titre, "(");
+			$heure_cours = substr($titre, $heure_cours);
+			$titre = substr($titre, 8);
+			$titre = substr($titre, 0, strpos($titre, "(") - strlen($titre));
+	?>
+
+
+
+    <header>
+
+        <h3><?= $titre  ?></h3>
+        <code> Code du cours :<?php the_field('sigle_du_cours'); ?></code>
+        <code> /Nombre d'heure :<?php the_field('duree_du_cours'); ?></code>
+        <code> /Nombre d'heure :<?php the_field('type_de_cours'); ?></code>
+    </header>
+    <?php the_content();
+
+			$le_permalien = "<a href='" . get_the_permalink() . "'>Suite</a>";
+
 			?>
 
-			 
 
-			<h1><?= $titre  ?></h1>
-			<code> Code du cours :<?= $code_cours  ?></code>
-			<code> /Nombre d'heure :<?= $heure_cours  ?></code>
-			<?php the_content();
-			
-			$le_permalien="<a href='".get_the_permalink()."'>Suite</a>";
-			
-			?>
-
-
-			<blockquote><?php the_excerpt() ?></blockquote><!--resume-->
-			<blockquote><?= wp_trim_words(get_the_excerpt(), 5,$le_permalien);	?></blockquote>
-			<pre><?php the_category();	?></pre>
-			<pre><?php the_date();	?></pre>
-			<pre><?php the_permalink();	?></pre>
-			<pre><?php the_author();	?></pre>
+    <blockquote><?php the_excerpt() ?></blockquote>
+    <!--resume-->
+    <blockquote><?= wp_trim_words(get_the_excerpt(), 5, $le_permalien);	?></blockquote>
+    <pre><?php the_category();	?></pre>
+    <pre><?php the_date();	?></pre>
+    <pre><?php the_permalink();	?></pre>
+    <pre><?php the_author();	?></pre>
 
 
 
 
 
 
-	<?php
+    <?php
 
 		endwhile;
 
