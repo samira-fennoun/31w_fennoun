dbc35b651720f57f42efe4b4d03dcb3981c20599
samira fennoun <101664372+e2295531@users.noreply.github.com>
2022-11-12 13:16:24 -0500

    ajout menu evenement

--- front-page.php
+++ front-page.php
@@ -19,6 +19,15 @@ get_header();
 <main class="site__main">
     <code>front-page.php</code>
     <?php
+    wp_nav_menu(array(
+        "menu" => "evenement",
+        "container" => "nav",
+        "container_class" => "menu_evenement"
+    ));
+
+
+
+
     if (have_posts()) :
         /* Start the Loop */
         while (have_posts()) :
