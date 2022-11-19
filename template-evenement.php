29651fdb3d05024e162b73d3fb8946f0ce12b1d4
samira fennoun <101664372+e2295531@users.noreply.github.com>
2022-11-19 12:38:15 -0500

    c15 modification template-evenement'

--- template-evenement.php
+++ template-evenement.php
@@ -11,9 +11,9 @@ get_header();
 
 
 <main class="site__main">
-    <h1>---- templete-evenement.php ------</h1>
+
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <?php the_title(); ?>
+    <h1><?php the_title(); ?></h1>
     <?php the_content(); ?>
     <br> adresse: <?php the_field('adresse'); ?> <br>date et heure:<?php the_field('date_et_heure_devenement'); ?>
     <?php endwhile ?> <?php endif ?>
