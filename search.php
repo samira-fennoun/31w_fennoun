79a1d4c4f35b0198cc0f373d95f2c49f8c2e64e6
Fennoun <e2295531@cmaisonneuve.qc.ca>
2022-12-13 16:10:13 -0500

    ef-3.a modification de page de search

---
+++ search.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package igc31w
+ */
+
+get_header();
+
+?>
+
+<main class="site__main">
+
+
+<h1>Résultat de la recherche</h1>
+<?php _e( 'Element de recherche', 'locale' ); ?>: "<?php the_search_query(); ?>"
+<?php
+    if (have_posts()) :
+
+        /* Start the Loop */
+        while (have_posts()) :
+            the_post();?>
+
+
+            
+       
+
+<div class="carte">
+
+    <header>
+        <?php the_post_thumbnail('thumbnail');   ?>
+    <?php     $titre = get_the_title();
+           
+           ?>
+        <h5><?= $titre  ?></h5>
+       
+    </header>
+    <?php
+
+            $le_permalien = "<a href='" . get_the_permalink() . "'>&#10132;</a>";
+
+            ?>
+
+
+    <p><?= wp_trim_words(get_the_excerpt(), 30, $le_permalien); ?></p>
+
+
+
+    </div>
+
+
+    <?php
+
+        endwhile;
+
+    endif;
+    ?>
+   <p>Nombre de Résultat:<?php echo $wp_query->found_posts; ?></p> 
+</main><!-- #main -->
+
+<?php
+
+get_footer();
